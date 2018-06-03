8a5df3160e7932442f2ebb0f2c298b47d71fae39
Jatin <Jatin@Jatin-PC>
2018-06-03 11:13:37 +0530

    1st commit in this project

---
+++ demo/src/test/java/pom/SearchPO.java
@@ -0,0 +1,22 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import library.BasePage;
+
+public class SearchPO extends BasePage
+{
+	public SearchPO(WebDriver driver)
+	{
+		super(driver);
+	}
+	@FindBy(xpath="//a[text()='Facebook - Log In or Sign Up']")
+	private WebElement facebookLink;
+	public WebElement facebookLink()
+	{
+		return facebookLink;
+	}
+	
+}
---
+++ demo/src/test/java/test/Test1.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+import library.BaseTest;
+import pom.GooglePO;
+import pom.SearchPO;
+
+public class Test1 extends BaseTest
+{
+	public WebDriverWait wait;
+	@Test
+	public void testFaceBookHome()
+	{
+		   wait=new WebDriverWait(driver, 20);
+			driver.get("https//google.com");
+			GooglePO googlePO=new GooglePO(driver);
+			googlePO.searchTxtBx().sendKeys("facebook" +Keys.ENTER);
+			SearchPO searchPO=new SearchPO(driver);
+			searchPO.verifyElement(searchPO.facebookLink());
+			searchPO.facebookLink().click();
+			WebDriverWait wait=new WebDriverWait(driver,10);
+			String eTitle="Facebook - log in or sign up";
+			wait.until(ExpectedConditions.titleContains(eTitle));
+			String aTitle=driver.getTitle();
+			Reporter.log("Title displayed", true);
+			Assert.assertEquals(aTitle, eTitle);
+			//driver.navigate().back();
+		
+	}
+}
